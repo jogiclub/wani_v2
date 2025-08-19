ce49f8edcb03828c641330943470ca3e65c137b2
jogiclub <jogiclub@gmail.com>
2025-08-19 12:45:11 +0900

    group-->org

--- application/controllers/Mypage.php
+++ application/controllers/Mypage.php
@@ -11,39 +11,37 @@ class Mypage extends CI_Controller
     }
 
 
-    public function index() {
+	public function index() {
+		if ($this->session->userdata('user_id')) {
+			$data['user'] = $this->session->userdata();
+			$user_id = $this->session->userdata('user_id');
+			$master_yn = $this->session->userdata('master_yn');
+			$this->load->model('User_model');
+			$data['user'] = $this->User_model->get_user_by_id($user_id);
 
-        if ($this->session->userdata('user_id')) {
-            $data['user'] = $this->session->userdata();
-            $user_id = $this->session->userdata('user_id');
-            $master_yn = $this->session->userdata('master_yn');
-            $this->load->model('User_model');
-            $data['user'] = $this->User_model->get_user_by_id($user_id);
+			$this->load->model('Org_model');
 
-            $this->load->model('Org_model');
+			if($master_yn === "N"){
+				$orgs = $this->Org_model->get_user_orgs($user_id);
+			} else {
+				$orgs = $this->Org_model->get_user_orgs_master($user_id);
+			}
 
-            if($master_yn === "N"){
-                $orgs = $this->Org_model->get_user_orgs($user_id);
-            } else {
-                $orgs = $this->Org_model->get_user_orgs_master($user_id);
-            }
+			foreach ($orgs as &$org) {
+				$org['user_count'] = $this->User_model->get_org_user_count($org['org_id']);
 
+				// 그룹에 대한 사용자의 level 값과 master_yn 값을 가져옴
+				$org['user_level'] = $this->User_model->get_org_user_level($user_id, $org['org_id']);
+				$org['user_master_yn'] = $this->session->userdata('master_yn');
+			}
 
-            foreach ($orgs as &$org) {
-                $org['user_count'] = $this->User_model->get_org_user_count($org['org_id']);
+			$data['orgs'] = $orgs;
 
-                // 그룹에 대한 사용자의 level 값과 master_yn 값을 가져옴
-                $org['user_level'] = $this->User_model->get_org_user_level($user_id, $org['org_id']);
-                $org['user_master_yn'] = $this->session->userdata('master_yn');
-            }
-
-            $data['orgs'] = $orgs;
-
-            $this->load->view('mypage', $data);
-        } else {
-            redirect('login');
-        }
-    }
+			$this->load->view('mypage', $data);
+		} else {
+			redirect('login');
+		}
+	}
 
 
 
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,132 +1,125 @@
 <?php
-class User_model extends CI_Model {
-    public function __construct() {
-        parent::__construct();
-        $this->load->database();
-    }
 
-    public function check_user($user_id) {
-        $query = $this->db->get_where('wb_user', array('user_id' => $user_id));
-        return $query->num_rows() > 0;
-    }
+class User_model extends CI_Model
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+	}
 
-    public function insert_user($data) {
-        $this->db->insert('wb_user', $data);
-        return $this->db->insert_id();
-    }
+	public function check_user($user_id)
+	{
+		$query = $this->db->get_where('wb_user', array('user_id' => $user_id));
+		return $query->num_rows() > 0;
+	}
 
-    public function get_user_orgs($user_id) {
-        $this->db->select('wb_org.idx as org_id, wb_org.org_name');
-        $this->db->from('wb_org');
-        $this->db->join('wb_org_user', 'wb_org.idx = wb_org_user.org_id');
-        $this->db->where('wb_org_user.user_id', $user_id);
-        $query = $this->db->get();
-        return $query->result_array();
-    }
+	public function insert_user($data)
+	{
+		$this->db->insert('wb_user', $data);
+		return $this->db->insert_id();
+	}
 
-
-    public function get_org_user_count($org_id) {
-        $this->db->where('wb_org_user.org_id', $org_id);
-        $this->db->from('wb_org_user');
-        $this->db->join('wb_user', 'wb_user.user_id = wb_org_user.user_id');
-        $this->db->where('wb_user.del_yn', 'N');
-        return $this->db->count_all_results();
-    }
+	public function get_user_orgs($user_id)
+	{
+		$this->db->select('wb_org.org_id as org_id, wb_org.org_name');
+		$this->db->from('wb_org');
+		$this->db->join('wb_org_user', 'wb_org.org_id = wb_org_user.org_id');
+		$this->db->where('wb_org_user.user_id', $user_id);
+		$query = $this->db->get();
+		return $query->result_array();
+	}
 
 
-    public function get_org_user_level($user_id, $org_id) {
-        $this->db->select('level');
-        $this->db->where('user_id', $user_id);
-        $this->db->where('org_id', $org_id);
-        $query = $this->db->get('wb_org_user');
-        $result = $query->row_array();
-        return $result ? $result['level'] : 0;
-    }
+	public function get_org_user_count($org_id)
+	{
+		$this->db->where('wb_org_user.org_id', $org_id);
+		$this->db->from('wb_org_user');
+		$this->db->join('wb_user', 'wb_user.user_id = wb_org_user.user_id');
+		$this->db->where('wb_user.del_yn', 'N');
+		return $this->db->count_all_results();
+	}
 
 
-    public function get_org_users($org_id) {
-        $this->db->select('wb_user.idx, wb_user.user_id, wb_user.user_name, wb_org_user.level, wb_user.user_mail, wb_user.user_hp, wb_user.master_yn');
-        $this->db->from('wb_user');
-        $this->db->join('wb_org_user', 'wb_user.user_id = wb_org_user.user_id');
-        $this->db->where('wb_org_user.org_id', $org_id);
-        $this->db->where('wb_user.del_yn', 'N');
-        $query = $this->db->get();
-        return $query->result_array();
-    }
+	public function get_org_user_level($user_id, $org_id)
+	{
+		$this->db->select('level');
+		$this->db->where('user_id', $user_id);
+		$this->db->where('org_id', $org_id);
+		$query = $this->db->get('wb_org_user');
+		$result = $query->row_array();
+		return $result ? $result['level'] : 0;
+	}
+
+	public function get_org_users($org_id)
+	{
+		$this->db->select('wb_user.idx, wb_user.user_id, wb_user.user_name, wb_org_user.level, wb_user.user_mail, wb_user.user_hp, wb_user.master_yn');
+		$this->db->from('wb_user');
+		$this->db->join('wb_org_user', 'wb_user.user_id = wb_org_user.user_id');
+		$this->db->where('wb_org_user.org_id', $org_id);
+		$this->db->where('wb_user.del_yn', 'N');
+		$query = $this->db->get();
+		return $query->result_array();
+	}
 
 
+	public function save_user($user_id, $user_name, $user_hp, $level, $org_id)
+	{
+		$data = array(
+			'user_name' => $user_name,
+			'user_hp' => $user_hp
+		);
 
+		$this->db->where('user_id', $user_id);
+		$this->db->update('wb_user', $data);
 
-    public function save_user($user_id, $user_name, $user_hp, $level, $org_id) {
-        $data = array(
-            'user_name' => $user_name,
-            'user_hp' => $user_hp
-        );
+		$org_user_data = array(
+			'level' => $level
+		);
 
-        $this->db->where('user_id', $user_id);
-        $this->db->update('wb_user', $data);
+		$this->db->where('user_id', $user_id);
+		$this->db->where('org_id', $org_id);
+		$result = $this->db->update('wb_org_user', $org_user_data);
 
-        $org_user_data = array(
-            'level' => $level
-        );
+		return $result;
+	}
 
-        $this->db->where('user_id', $user_id);
-        $this->db->where('org_id', $org_id);
-        $result = $this->db->update('wb_org_user', $org_user_data);
+	public function delete_user($user_id, $org_id)
+	{
+		$data = array(
+			'del_yn' => 'Y',
+			'del_date' => date('Y-m-d H:i:s')
+		);
 
-        return $result;
-    }
+		$this->db->where('user_id', $user_id);
+		$this->db->update('wb_user', $data);
 
+		$this->db->where('user_id', $user_id);
+		$this->db->where('org_id', $org_id);
+		$this->db->delete('wb_org_user');
 
-    public function delete_user($user_id, $org_id) {
-        $data = array(
-            'del_yn' => 'Y',
-            'del_date' => date('Y-m-d H:i:s')
-        );
+		return $this->db->affected_rows() > 0;
+	}
 
-        $this->db->where('user_id', $user_id);
-        $this->db->update('wb_user', $data);
+	public function insert_org_user($data)
+	{
+		$this->db->insert('wb_org_user', $data);
+		return $this->db->insert_id();
+	}
 
-        $this->db->where('user_id', $user_id);
-        $this->db->where('org_id', $org_id);
-        $this->db->delete('wb_org_user');
+	public function get_org_user($user_id, $org_id)
+	{
+		$this->db->where('user_id', $user_id);
+		$this->db->where('org_id', $org_id);
+		$query = $this->db->get('wb_org_user');
+		return $query->row_array();
+	}
 
-        return $this->db->affected_rows() > 0;
-    }
-
-
-
-
-
-    public function get_user_by_id($user_id) {
-        $this->db->where('user_id', $user_id);
-        $query = $this->db->get('wb_user');
-        return $query->row_array();
-    }
-
-
-    public function update_user($user_id, $data) {
-        $this->db->where('user_id', $user_id);
-        $this->db->update('wb_user', $data);
-    }
-
-
-    public function insert_org_user($data) {
-        $this->db->insert('wb_org_user', $data);
-        return $this->db->insert_id();
-    }
-
-    public function get_org_user($user_id, $org_id) {
-        $this->db->where('user_id', $user_id);
-        $this->db->where('org_id', $org_id);
-        $query = $this->db->get('wb_org_user');
-        return $query->row_array();
-    }
-
-    public function get_user_by_email($email) {
-        $this->db->where('user_mail', $email);
-        $query = $this->db->get('wb_user');
-        return $query->row_array();
-    }
+	public function get_user_by_email($email)
+	{
+		$this->db->where('user_mail', $email);
+		$query = $this->db->get('wb_user');
+		return $query->row_array();
+	}
 
 }
