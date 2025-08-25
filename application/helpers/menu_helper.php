c3199967ee5d34b89e5f72bb28625c81a6978515
jogiclub <jogiclub@gmail.com>
2025-08-25 11:40:07 +0900

    메뉴 상수 관리, 사용자 관리 모달 업데이트

---
+++ application/helpers/menu_helper.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * 파일 위치: application/helpers/menu_helper.php
+ * 역할: 메뉴 관련 헬퍼 함수
+ */
+
+if (!function_exists('get_system_menus')) {
+    /**
+     * 시스템 메뉴 배열 반환
+     */
+    function get_system_menus() {
+        return array(
+            'OVERVIEW' => array(
+                'name' => '대시보드',
+                'key' => 'OVERVIEW',
+                'url' => 'main',
+                'icon' => 'bi bi-file-earmark-ruled',
+                'level' => 0
+            ),
+            'MEMBER_MANAGEMENT' => array(
+                'name' => '회원관리',
+                'key' => 'MEMBER_MANAGEMENT',
+                'url' => 'member',
+                'icon' => 'bi bi-people',
+                'level' => 1
+            ),
+            'ATTENDANCE_MANAGEMENT' => array(
+                'name' => '출석관리',
+                'key' => 'ATTENDANCE_MANAGEMENT',
+                'url' => 'attendance',
+                'icon' => 'bi bi-clipboard-check',
+                'level' => 1
+            ),
+            'ATTENDANCE_BOARD' => array(
+                'name' => '판',
+                'key' => 'ATTENDANCE_BOARD',
+                'url' => 'main',
+                'icon' => 'bi bi-person-check',
+                'level' => 1
+            ),
+            'WEEKLY_STATISTICS' => array(
+                'name' => '주별통계',
+                'key' => 'WEEKLY_STATISTICS',
+                'url' => 'statistics/weekly',
+                'icon' => 'bi bi-graph-up-arrow',
+                'level' => 1
+            ),
+            'MEMBER_STATISTICS' => array(
+                'name' => '회원별통계',
+                'key' => 'MEMBER_STATISTICS',
+                'url' => 'statistics/member',
+                'icon' => 'bi bi-clipboard-data',
+                'level' => 1
+            ),
+            'ORG_SETTING' => array(
+                'name' => '조직설정',
+                'key' => 'ORG_SETTING',
+                'url' => 'org',
+                'icon' => 'bi bi-building-gear',
+                'level' => 9
+            ),
+            'GROUP_SETTING' => array(
+                'name' => '그룹설정',
+                'key' => 'GROUP_SETTING',
+                'url' => 'member_area',
+                'icon' => 'bi bi-diagram-3',
+                'level' => 9
+            ),
+            'DETAIL_FIELD_SETTING' => array(
+                'name' => '상세필드설정',
+                'key' => 'DETAIL_FIELD_SETTING',
+                'url' => 'detail_field',
+                'icon' => 'bi bi-input-cursor-text',
+                'level' => 9
+            ),
+            'ATTENDANCE_SETTING' => array(
+                'name' => '출석설정',
+                'key' => 'ATTENDANCE_SETTING',
+                'url' => 'attendance_setting',
+                'icon' => 'bi bi-sliders2-vertical',
+                'level' => 9
+            ),
+            'USER_MANAGEMENT' => array(
+                'name' => '사용자관리',
+                'key' => 'USER_MANAGEMENT',
+                'url' => 'user_management',
+                'icon' => 'bi bi-person-video',
+                'level' => 9
+            )
+        );
+    }
+}
+
+if (!function_exists('get_menu_categories')) {
+    /**
+     * 메뉴 카테고리 반환
+     */
+    function get_menu_categories() {
+        return array(
+            'OVERVIEW' => array('OVERVIEW'),
+            'MEMBER' => array('MEMBER_MANAGEMENT', 'ATTENDANCE_MANAGEMENT', 'ATTENDANCE_BOARD'),
+            'STATICS' => array('WEEKLY_STATISTICS', 'MEMBER_STATISTICS'),
+            'SETTING' => array('ORG_SETTING', 'GROUP_SETTING', 'DETAIL_FIELD_SETTING', 'ATTENDANCE_SETTING', 'USER_MANAGEMENT')
+        );
+    }
+}
---
+++ application/views/header_meta.php
@@ -0,0 +1,19 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="왔니" />
+<meta name="keywords" content="출석 체크, 교적, 메모, 심방" />
+<meta name="author" content="wani.im" />
+
+<!-- Facebook and Twitter integration -->
+<meta property="og:title" content="왔니"/>
+<meta property="og:image" content=""/>
+<meta property="og:url" content=""/>
+<meta property="og:site_name" content="왔니"/>
+<meta property="og:description" content="왔니"/>
+
+<meta name="twitter:title" content="왔니" />
+<meta name="twitter:image" content="" />
+<meta name="twitter:url" content="wani.im" />
+<meta name="twitter:card" content="왔니" />
