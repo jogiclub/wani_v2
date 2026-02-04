de13953a4ca58e86d79a5d728bb424f7a2aa8291
jogiclub <jogiclub@gmail.com>
2026-02-04 19:45:57 +0900

    Apply .gitignore to application/config

--- application/config/surem.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * 파일 위치: application/config/surem.php
- * 역할: 슈어엠 API 설정
- */
-defined('BASEPATH') or exit('No direct script access allowed');
-
-$config['surem_user_code'] = 'webhows'; // 슈어엠 제공 아이디
-$config['surem_secret_key'] = 'c81m0wt5chdqqxh1fr2hri5g4qizpzzjc2058iuv'; // 슈어엠 제공 API 키
